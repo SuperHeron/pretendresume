e5debab127acf7bd412e78343b94e75263591307
Pierre Lejeune <superheron@gmail.com>
2009-03-25 22:12:08 +0100

    first commit

---
+++ Makefile
@@ -0,0 +1,29 @@
+PALUDIS_HOOK_NAME=install_pretend_post
+PALUDIS_HOOK_SONAME=PretendResume
+
+SRC=$(wildcard *.cc)
+OBJ=$(SRC:.cc=.o)
+HEADERS=$(wildcard *.hh *.h)
+
+all: objbindir PALUDIS_HOOK_SONAME.so
+
+PALUDIS_HOOK_SONAME.so: $(OBJ)
+	g++ -shared obj/*.o $(LDFLAGS) `pkg-config --libs paludis` -o bin/$(PALUDIS_HOOK_SONAME).so
+
+%.o: %.cc $(HEADERS)
+	g++ -Wall $(CXXFLAGS) `pkg-config --cflags paludis` -fPIC -c $< -o obj/$@
+
+objbindir:
+	mkdir obj bin
+
+install:
+	mkdir -p $(DESTDIR)/usr/share/paludis/hooks/$(PALUDIS_HOOK_NAME)
+	cp bin/$(PALUDIS_HOOK_SONAME).so $(DESTDIR)/usr/share/paludis/hooks/$(PALUDIS_HOOK_NAME)/$(PALUDIS_HOOK_SONAME)_$(PALUDIS_HOOK_SUFFIX)
+
+.PHONY: clean mrproper
+
+clean:
+	rm -f obj/*.o
+
+mrproper: clean
+	rm -f bin/$(PALUDIS_HOOK_SONAME).so
---
+++ PretendResume.cc
@@ -0,0 +1,110 @@
+/*
+ * Copyright (C) 2008 Pierre Lejeune
+ *
+ * This source file is intended to be compiled as a shared library
+ * to be used as a hook for Paludis, the other Package Mangler.
+ * It outputs the resume command on the console or in a file regarding the "resume-command-template" argument.
+ * To use it, copy it or make a link to it into "${SHAREDIR}/paludis/install_pretend_post".
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+//#include <cctype>
+#include <cstdlib>
+#include <cstring>
+#include <sys/stat.h>
+
+#include <paludis/hook.hh>
+#include <paludis/util/system.hh>
+//#include <paludis/util/wrapped_forward_iterator.hh>
+
+/**
+ * Function to run the current hook (declared in Paludis API)
+ */
+paludis::HookResult paludis_hook_run(const paludis::Environment* env, const paludis::Hook& hook)
+{
+    paludis::HookResult result = { paludis::value_for<paludis::n::max_exit_status>(0), paludis::value_for<paludis::n::output>("") };
+    int fd = 0;
+/*
+ * Showing all variables in hook
+ * For debugging only
+ */
+//	for(paludis::Hook::ConstIterator h(hook.begin()), h_end(hook.end()); h != h_end; ++h)
+//		std::cout << h->first << " : " << h->second << std::endl;
+// Check whether $RESUME_COMMAND is defined
+    if(hook.get("RESUME_COMMAND").empty())
+    {
+        result.output = paludis::value_for<paludis::n::output>("No resume command");
+        return result;
+    }
+    else
+    {
+        std::cout << std::endl;
+// Check whether there are errors in deplist
+        if(hook.get("DEPLIST_HAS_ERRORS") == "false")
+        {
+// Check whether resume_command_template argument is set
+            std::string paludis_client = paludis::getenv_or_error("PALUDIS_CLIENT");
+            std::transform(paludis_client.begin(), paludis_client.end(), paludis_client.begin(), toupper);
+            std::string rctValue = paludis::getenv_with_default(paludis_client + "_CMDLINE_resume_command_template", "null");
+// Output resume command to console
+            if(rctValue == "null")
+            {
+                std::cout << "Resume command: " << hook.get("RESUME_COMMAND") << std::endl;
+                result.output = paludis::value_for<paludis::n::output>("Resume Command OK");
+                return result;
+            }
+// Output resume command to file
+            else
+            {
+                std::string rctFile = rctValue;
+                if(rctFile.find("XXXXXX") != std::string::npos)
+                {
+                    char* rctFileCStr = new char[rctFile.length() + 1];
+                    memset(rctFileCStr, '\0', rctFile.length() + 1);
+                    rctFile.copy(rctFileCStr, rctFile.length());
+                    fd = mkstemp(rctFileCStr);
+                    rctFile = rctFileCStr;
+                }
+                std::fstream rctFileStream(rctFile.c_str());
+                if(rctFileStream)
+                {
+                    rctFileStream << hook.get("RESUME_COMMAND") << std::endl;
+                    rctFileStream.close();
+                    chmod(rctFile.c_str(), 0644);
+                    std::cout << "Resume command saved to file: " << rctFile << std::endl;
+                    result.output = paludis::value_for<paludis::n::output>("Resume Command Saved To File");
+                    return result;
+                }
+                else
+                {
+                    std::cout << "Resume command NOT saved to file: " << rctFile << std::endl;
+                    std::cout << "Resume command: " << hook.get("RESUME_COMMAND") << std::endl;
+                    result.output = paludis::value_for<paludis::n::output>("Resume Command NOT Saved To File");
+                    return result;
+                }
+            }
+        }
+// Error
+        else
+        {
+            std::cout << "Error in resume command" << std::endl;
+            result.output = paludis::value_for<paludis::n::output>("Error in resume command");
+            return result;
+        }
+    }
+}
